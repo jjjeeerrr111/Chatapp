3d37a3f862532ddf8a2e3d0585a55be42e002c07
Jeremy Sharvit <sharvitjeremy@gmail.com>
2016-04-17 14:19:13 +0700

    make message view move up and down with keyboard display/hiding

--- WhaleTalk/ChatViewController.swift
+++ WhaleTalk/ChatViewController.swift
@@ -14,8 +14,8 @@ class ChatViewController: UIViewController {
     
     private var messages = [Message]()
     private let cellIdentifier = "Cell"
-    
-    
+    private let newMessageField = UITextView()
+    private var bottomConstraint : NSLayoutConstraint!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -32,6 +32,27 @@ class ChatViewController: UIViewController {
             
         }
         
+        let newMessageArea = UIView()
+        newMessageArea.backgroundColor = UIColor.lightGrayColor()
+        newMessageArea.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(newMessageArea)
+        
+        newMessageField.translatesAutoresizingMaskIntoConstraints = false
+        newMessageField.scrollEnabled = false
+        newMessageArea.addSubview(newMessageField)
+        
+        let sendButton = UIButton()
+        sendButton.translatesAutoresizingMaskIntoConstraints = false
+        sendButton.setTitle("Send", forState: .Normal)
+        sendButton.setContentHuggingPriority(251, forAxis: .Horizontal)
+        newMessageArea.addSubview(sendButton)
+        
+        bottomConstraint = newMessageArea.bottomAnchor.constraintEqualToAnchor(view.bottomAnchor)
+        bottomConstraint.active = true
+        
+        let messageAreaConstaints : [NSLayoutConstraint] = [ newMessageArea.leadingAnchor.constraintEqualToAnchor(view.leadingAnchor), newMessageArea.trailingAnchor.constraintEqualToAnchor(view.trailingAnchor), newMessageField.leadingAnchor.constraintEqualToAnchor(newMessageArea.leadingAnchor, constant: 10), newMessageField.centerYAnchor.constraintEqualToAnchor(newMessageArea.centerYAnchor), sendButton.trailingAnchor.constraintEqualToAnchor(newMessageArea.trailingAnchor, constant: -10), newMessageField.trailingAnchor.constraintEqualToAnchor(sendButton.leadingAnchor, constant: -10), sendButton.centerYAnchor.constraintEqualToAnchor(newMessageField.centerYAnchor), newMessageArea.heightAnchor.constraintEqualToAnchor(newMessageField.heightAnchor, constant:20)]
+        
+        NSLayoutConstraint.activateConstraints(messageAreaConstaints)
         
         tableView.registerClass(ChatCell.self, forCellReuseIdentifier: cellIdentifier)
         tableView.translatesAutoresizingMaskIntoConstraints = false
@@ -41,11 +62,49 @@ class ChatViewController: UIViewController {
         
         view.addSubview(tableView)
         
-        let tableViewConstraints : [NSLayoutConstraint] = [tableView.topAnchor.constraintEqualToAnchor(view.topAnchor), tableView.leadingAnchor.constraintEqualToAnchor(view.leadingAnchor), tableView.trailingAnchor.constraintEqualToAnchor(view.trailingAnchor), tableView.bottomAnchor.constraintEqualToAnchor(view.bottomAnchor)];
+        let tableViewConstraints : [NSLayoutConstraint] = [tableView.topAnchor.constraintEqualToAnchor(view.topAnchor), tableView.leadingAnchor.constraintEqualToAnchor(view.leadingAnchor), tableView.trailingAnchor.constraintEqualToAnchor(view.trailingAnchor), tableView.bottomAnchor.constraintEqualToAnchor(newMessageArea.topAnchor)];
         
         NSLayoutConstraint.activateConstraints(tableViewConstraints)
+        
+        //add nsnotification center listener for keyboard popping up
+        NSNotificationCenter.defaultCenter().addObserver(self, selector:#selector(self.keyboardWillShow(_:)), name: UIKeyboardWillShowNotification, object: nil)
+        
+        //add nsnotification center listener for keyboard going down
+        NSNotificationCenter.defaultCenter().addObserver(self, selector:#selector(self.keyboardWillHide(_:)), name: UIKeyboardWillHideNotification, object: nil)
+        
+        //add UITapGesture to close keyboard
+        let tapRecognizer = UITapGestureRecognizer(target: self, action: #selector(self.handleSingleTap(_:)))
+        tapRecognizer.numberOfTapsRequired = 1
+        view.addGestureRecognizer(tapRecognizer)
     }
 
+    
+    func handleSingleTap(recognizer: UITapGestureRecognizer) {
+        view.endEditing(true)
+    }
+    
+    func updateBottomConstaint(notification: NSNotification) {
+        if let userInfo = notification.userInfo, frame = userInfo[UIKeyboardFrameEndUserInfoKey]?.CGRectValue,animationDuration = userInfo[UIKeyboardAnimationDurationUserInfoKey]?.doubleValue {
+            
+            let newFrame = view.convertRect(frame, fromView: (UIApplication.sharedApplication().delegate?.window)!)
+            
+            bottomConstraint.constant = newFrame.origin.y - CGRectGetHeight(view.frame)
+            
+            UIView.animateWithDuration(animationDuration, animations: {
+                self.view.layoutIfNeeded()
+            })
+        }
+    }
+    //this will move the newMessageArea up when keyboard is shown by redrawing the view
+    func keyboardWillShow(notification: NSNotification) {
+        self.updateBottomConstaint(notification)
+    }
+    
+    //this will move the newMessageArea down when keyboard will hide by redrawing the view
+    func keyboardWillHide(notification: NSNotification) {
+        self.updateBottomConstaint(notification)
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
